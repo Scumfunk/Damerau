fbd76e391898027dfa063684461cc1c55eb8f6ad
Yury Ivannikov <feelzn0@gmail.com>
2015-11-10 18:20:55 +0300

    added missing files

--- lib/Damerau.rb
+++ lib/Damerau.rb
@@ -1,5 +1,52 @@
 require "Damerau/version"
 
 module Damerau
-  # Your code goes here...
+  #Returns Damerau-Levinshtein distance between two strings
+  #Usage: distance(string1,string2)
+
+  def self.distance(a,b)
+  	unless ((a.class.ancestors.include?(String)) and (b.class.ancestors.include?(String)))
+  		raise "arguments must be String or child"
+  	end
+  	get_distance(a,b,a.size,b.size)
+  end
+
+  private
+
+  #recursive function
+  def self.get_distance (a, b, i, j)
+  	if min(i,j) == 0
+  	  max(i,j)
+  	elsif ((i>1) and (j>1) and (at(a,i)==at(b,j-1)) and (at(a,i-1)==at(b,j)))
+  	  min(get_distance(a,b,i-1,j)+1,
+  	  	  get_distance(a,b,i,j-1)+1,
+  	  	  get_distance(a,b,i-1,j-1)+ai_not_equal_bj_indicator(a,b,i,j),
+  	  	  get_distance(a,b,i-2,j-2)+1)
+  	else
+	  min(get_distance(a,b,i-1,j)+1,
+  	  	  get_distance(a,b,i,j-1)+1,
+  	      get_distance(a,b,i-1,j-1)+ai_not_equal_bj_indicator(a,b,i,j))
+  	end
+  end
+
+  #small hack: .at normalizes numeration in strings(from 0 in ruby, from 1 in algorythm)
+  def self.at(arr,i)
+  	arr[i-1]
+  end
+
+  def self.min (*argv)
+  	argv.min
+  end
+
+  def self.max (*argv)
+  	argv.max
+  end
+
+  def self.ai_not_equal_bj_indicator (a,b,i,j)
+  	if at(a,i)==at(b,j)
+  	  0
+  	else
+  	  1
+  	end
+  end
 end
--- spec/Damerau_spec.rb
+++ spec/Damerau_spec.rb
@@ -5,7 +5,46 @@ describe Damerau do
     expect(Damerau::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+   it 'should raise an exeprion when arguments is not String objects' do
+    expect{Damerau.distance(1, 0.5)}.to raise_error()
+  end
+
+  it '.distance(a,b) should return distance between a and b' do
+    Damerau.methods.include?(:distance).should eq(true)
+  end
+
+  it 'should calculate insertions/delitions correctly' do
+  	string1 = ('a'..'f').to_a.join
+  	string2 = 'a'
+  	Damerau.distance(string1,string2).should eq(string1.size-1)
+  end
+
+  it 'should calculate replacements correctly' do
+  	string1 = ('a'..'f').to_a.join
+  	string2 = ('g'..'l').to_a.join
+  	Damerau.distance(string1,string2).should eq(string1.size)
+  end
+
+  it 'should calculate swaps correctly' do
+  	string1 = 'af'
+  	string2 = 'fa'
+  	Damerau.distance(string1,string2).should eq(1)
+  end
+
+  it '.min(*argv) should return min from arguments array' do
+  	Damerau.min(1,2,3,4,5,6,7,8,9,0).should eq(0)
+  end
+
+  it '.max(*argv) should return max from arguments array' do
+  	Damerau.max(1,2,3,4,5,6,7,8,9,0).should eq(9)
+  end
+
+  it '.ai_not_equal_bj_indicator(string1,string2,i,j) should return 0 or 1' do
+  	Damerau.ai_not_equal_bj_indicator('ab','fb',2,2).should eq(0)
+  	Damerau.ai_not_equal_bj_indicator('ab','fb',2,1).should eq(1)
+  end
+
+  it '.get_distance(string1,string2,i,j) should return distance' do
+  	Damerau.get_distance('afafaf','fafafa',6,6).should eq(2)
   end
 end
